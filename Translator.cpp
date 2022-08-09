7e085e2d17e696f4d8d63c470ad738b7d8cc945a
shintaroDesu <snkraishin87@gmail.com>
2022-08-09 12:36:08 +0200

    wip

--- Translator.cpp
+++ Translator.cpp
@@ -2,16 +2,20 @@
 // Created by yuitora . on 08/08/2022.
 //
 
+#include <string>
+#include <boost/locale.hpp>
 #include "Translator.h"
 #include "utils.h"
 
 Translator::Translator(Language from_lang, Language to_lang) {
+    curl = curl_easy_init();
     this->from_lang = from_lang;
     this-> to_lang = to_lang;
     this->init();
 }
 
 Translator::Translator() {
+    curl = curl_easy_init();
     this->from_lang = English;
     this->to_lang = English;
     this->init();
@@ -29,7 +33,7 @@ static size_t writeCallback(void *contents, size_t size, size_t nmemb, void *use
 
 
 
-std::string Translator::translate_wiki(std::string word) {
+std::string Translator::translate_wiki(const std::string& word) {
     if (!curl) {return "";}
 
     std::string pageid = this->get_pageid(word);
@@ -56,7 +60,7 @@ std::string Translator::translate_wiki(std::string word) {
     return readBuffer;
 }
 
-std::string Translator::get_pageid(std::string word) {
+std::string Translator::get_pageid(const std::string& word) {
     if (!curl) {return "";}
 
     std::string readBuffer;
@@ -69,7 +73,7 @@ std::string Translator::get_pageid(std::string word) {
               {"format", "json"},
               {"srsearch", param_word},
               {"srwhat", "text"},
-              {"srlimit", "20"}};
+              {"srlimit", "100"}};
 
     std::string url = addParameters(this->wiki_url, p);
     std::cout << url << std::endl;
@@ -80,14 +84,58 @@ std::string Translator::get_pageid(std::string word) {
 
     curl_easy_perform(curl);
 
-    return readBuffer;
+    json data = json::parse(readBuffer.c_str());
+
+    std::string pageid = this->find_right_pageid(data);
+
+    return pageid;
 }
 
 void Translator::init() {
-    curl = curl_easy_init();
     this->langCode = LanguageCode[from_lang];
     std::stringstream ss;
     ss << "https://" << this->langCode << ".wikipedia.org/w/api.php";
     this->wiki_url.assign(ss.str());
+
+    this->get_math_word();
+}
+
+std::string Translator::find_right_pageid(const json &data) {
+
+    json results_array = data["query"]["search"];
+    int array_size = results_array.size();
+
+    for (int i = 0; i < array_size; i++) {
+
+    }
+
+    return to_string(data["query"]["search"][2]["pageid"]);
+}
+
+void Translator::get_math_word() {
+
+    if (this->from_lang == English) {this->math_word.assign("Mathematics");}
+
+    Params p {{"action", "query"},
+              {"format", "json"},
+              {"prop", "langlinks"},
+              {"pageids", MATHS_PAGEID_EN},
+              {"lllang", LanguageCode[this->from_lang]},
+              {"lllimit", "100"}};
+
+    std::string readBuffer;
+    std::string url = addParameters("https://en.wikipedia.org/w/api.php", p);
+
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+
+    curl_easy_perform(curl);
+
+
+
+    json data = json::parse(readBuffer.c_str());
+
+    this->math_word = data["query"]["pages"][MATHS_PAGEID_EN]["langlinks"][0]["*"];
 }
 
--- Translator.h
+++ Translator.h
@@ -9,6 +9,9 @@
 #include <iostream>
 #include <curl/curl.h>
 #include "Languages.h"
+#include "jsonparser/json.h"
+
+using json = nlohmann::json;
 
 class Translator {
 private:
@@ -16,9 +19,19 @@ private:
     std::string langCode;
     std::string wiki_url;
 
+    const std::string MATHS_PAGEID_EN = "18831";
+
     CURL *curl;
 
     void init();
+
+    std::string get_pageid(const std::string& word);
+
+    std::string find_right_pageid(const json &data);
+
+    void get_math_word();
+
+    std::string math_word;
 
 public:
     Translator(Language from_lang, Language to_lang);
@@ -26,9 +39,7 @@ public:
 
     ~Translator();
 
-    std::string get_pageid(std::string word);
-
-    std::string translate_wiki(std::string word);
+    std::string translate_wiki(const std::string& word);
 
 };
 
